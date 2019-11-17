c38978c10c2f2abf2b500f544ec698d5cc9009d0
Ana Robalo <anameciarobalo@gmail.com>
2019-11-17 20:06:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |trait,attribute|
+    attribute.each |detail,names| do
+      names.each |n| do
+        pigeon_list[name] = 
+    end
+      
+    
+  end
+  
 end
