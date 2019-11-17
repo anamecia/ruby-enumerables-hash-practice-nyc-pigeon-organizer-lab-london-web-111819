1e750d3182976f83fffbbbcaeacab49e388c5bcc
Ana Robalo <anameciarobalo@gmail.com>
2019-11-17 20:27:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,10 +4,10 @@ def nyc_pigeon_organizer(data)
   data.each do |trait,attribute|
     attribute.each do |detail,names|
       names.each do |name|
-        if pigeon_hash[name]
+        if !pigeon_hash[name] 
           pigeon_hash[name] = {}
         end
-        if pigeon_hash[name][trait]
+        if !pigeon_hash[name][trait]
           pigeon_hash[name][trait] = []
         end
         pigeon_hash[name][trait].push(detail.to_s)
